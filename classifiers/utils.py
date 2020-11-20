Fix data leak in compas

classifiers/utils.py
@@ -57,21 +57,26 @@ def batch_weights(sample_size, batch_size):
 def weighted_mean(values, weights):
     return float(torch.sum(torch.Tensor(values) * weights))
 
-def all_binaries(domain_spec):
+def all_binaries(domain_spec, threshold=[]):
     bins = [[]]
     counter = 0
-    for spec in domain_spec:
+    for i, spec in enumerate(domain_spec):
         new_bins = []
-        
-        if spec == 1:
-            for b in bins:
-                new_bins.append(tuple(list(b) + [0]))
-                new_bins.append(tuple(list(b) + [1]))
+        if i in threshold:
+            for j in range(spec+1):
+                for b in bins:
+                    new_bins.append(tuple(list(b) + [1] * j + [0] * (spec - j)))
+
         else:
-            vals = [list([int(i) for i in t]) for t in torch.eye(spec)]
-            for b in bins:
-                for v in vals:
-                    new_bins.append(tuple(list(b) + v))
+            if spec == 1:
+                for b in bins:
+                    new_bins.append(tuple(list(b) + [0]))
+                    new_bins.append(tuple(list(b) + [1]))
+            else:
+                vals = [list([int(i) for i in t]) for t in torch.eye(spec)]
+                for b in bins:
+                    for v in vals:
+                        new_bins.append(tuple(list(b) + v))
 
         bins = new_bins
     return bins

densities/empirical.py
@@ -1,3 +1,4 @@
+#%%
 import math
 import torch
 
@@ -6,17 +7,20 @@ from torch.distributions.uniform import Uniform
 
 class EmpiricalDistribution(Distribution):
 
-    def __init__(self, raw_samples, domain=None, baseline=0):
-
-        samples = list(tuple(int(x) for x in v) for v in raw_samples)
+    def __init__(self, raw_samples, domain=None, probs=None, baseline=0):
 
         self.domain = set(samples) if domain is None else domain
-        self.probs = {k: 0 for k in self.domain}
-        for s in samples:
-            self.probs[s] += 1
 
-        for s in self.domain:
-            self.probs[s] = (self.probs[s] + baseline) / (len(samples) + len(self.domain) * baseline)
+        if probs:
+            self.probs = probs
+        else:
+            samples = list(tuple(int(x) for x in v) for v in raw_samples)
+            self.probs = {k: 0 for k in self.domain}
+            for i, s in enumerate(samples):
+                self.probs[s] += 1
+
+            for s in self.domain:
+                self.probs[s] = (self.probs[s] + baseline) / (len(samples) + len(self.domain) * baseline)
 
     def log_prob(self, value):
         t_value = tuple(int(v) for v in value)
@@ -37,4 +41,5 @@ class EmpiricalDistribution(Distribution):
 
         ids = torch.sum(cdf_vector.repeat(n_samples, 1) < u_samples.repeat(len(cdf_vector), 1).T, axis=1)
 
-        return(torch.Tensor([val_vec[i] for i in ids]).reshape(*sample_shape, -1))
+        return (torch.Tensor([val_vec[i] for i in ids]).reshape(*sample_shape, -1))
+# %%

densities/fairdensity.py
@@ -2,11 +2,17 @@
 import math
 import torch
 import itertools
+import torch.distributions as D
+
+from densities.empirical import EmpiricalDistribution
 
 from scipy.integrate import nquad
 from torch.distributions.multinomial import Multinomial
+from torch.distributions.distribution import Distribution
 
-class FairDensity:
+#%%
+
+class FairDensity(Distribution):
 
     def __init__(self, q_init_xa_conds, x_support, a_domain):
         self.q_init_xa_conds = q_init_xa_conds
@@ -18,6 +24,7 @@ class FairDensity:
 
         self.a_pdf = Multinomial(probs=torch.ones(len(a_domain)))
 
+
     def normalise(self, abstol=1e-3):
         z = 0
         for a in self.a_domain:
@@ -26,6 +33,7 @@ class FairDensity:
                 z_a += math.exp(self._unnorm_log_prob(torch.Tensor(x), a))
 
             z += z_a
+
 
         return math.log(z)
 
@@ -36,6 +44,7 @@ class FairDensity:
 
         return self._unnorm_log_prob(x, a) - norm
 
+
     def _unnorm_log_prob(self, x, a):
         ai = self.a_domain.index(a)
         dens = self.q_init_xa_conds[ai].log_prob(x) - math.log(len(self.a_domain))
@@ -45,10 +54,12 @@ class FairDensity:
 
         return dens
 
+
     def append(self, m, theta):
         self.models.append(m)
         self.thetas.append(theta)
         self.logzs.append(self.normalise())
+
 
     def gen_loss_function(self):
         def loss(classifier, samples):
@@ -75,7 +86,8 @@ class FairDensity:
             return -(p_expectation + q_expectation)
 
         return loss
-    
+
+
     def representation_rate(self):
         rr_list = []
 
@@ -107,9 +119,46 @@ class FairDensity:
 
         return sample_list
 
+
     def get_prob_array(self):
         probs = []
         for x, a in itertools.product(self.x_support, self.a_domain):
             probs.append(math.exp(self.log_prob(torch.Tensor(x), a)))
 
         return probs
+
+
+    def rsample(self, sample_shape=torch.Size([])):
+        a_sampler = D.OneHotCategorical(probs=torch.ones(len(self.a_domain)))
+
+        probs = {}
+        x_a_dists = {}
+        for a in self.a_domain:
+            probs[a] = {}
+            for x in self.x_support:
+                probs[a][x] = math.exp(self.log_prob(x, a))
+
+            normalise = sum(probs[a].values())
+            for x in self.x_support:
+                probs[a][x] = probs[a][x] / normalise
+
+            x_a_dists[a] = EmpiricalDistribution(None, domain=self.x_support, probs=probs[a])
+            self.test = EmpiricalDistribution(None, domain=self.x_support, probs=probs[a])
+
+        a_vals = a_sampler.sample_n(sample_shape.numel())
+
+        a_counts = torch.sum(a_vals, axis=0)
+
+        x_samples = []
+        a_samples = []
+        for a_c, a_vals in zip(a_counts, self.a_domain):
+            a_c = int(a_c)
+            x_samples.append(x_a_dists[a_vals].sample_n(a_c))
+            a_samples.append(torch.Tensor([a_vals] * a_c))
+
+        x_samples = torch.cat(x_samples).view(*sample_shape, -1)
+        a_samples = torch.cat(a_samples).view(*sample_shape, -1)
+
+        return x_samples, a_samples
+
+# %%

run_experiments_adult.py
@@ -98,7 +98,7 @@ for f_idx, (train_index, test_index) in enumerate(cv.split(dataset_df)):
 
     #%%
     # Fair Density setup
-    x_support = all_binaries(DOMAIN)
+    x_support = all_binaries(DOMAIN, threshold=[1,2])
     a_domain = [tuple(int(v) for v in t) for t in torch.eye(train_a.shape[1])]
 
     train_x_cond_a = []

run_experiments_compas_small.py
@@ -20,6 +20,14 @@ from aif360.algorithms.preprocessing.optim_preproc_helpers.data_preproc_function
 from aif360.datasets import CompasDataset
 
 #%%
+def interval(row):
+    i_loc = np.where(row == 1)[0]
+    if len(i_loc) > 0:
+        for i in range(i_loc[0]):
+            row[i] = 1
+    return row
+
+#%%
 TAU = float(sys.argv[1])  # 0.9
 SENSITIVE_ATTRIBUTE = str(sys.argv[2])  # 'sex'
 
@@ -32,6 +40,14 @@ NAME = 'compas_small_{}_{}.json'.format(SENSITIVE_ATTRIBUTE, TAU)
 
 dataset = load_preproc_data_compas(['sex'])
 dataset_df = dataset.convert_to_dataframe()[0]
+# 
+dataset_df = dataset_df.reindex(columns=['sex', 'race', 'age_cat=Less than 25', 'age_cat=25 to 45', 'age_cat=Greater than 45', 'priors_count=0', 'priors_count=1 to 3', 'priors_count=More than 3', 'c_charge_degree=F', 'c_charge_degree=M','two_year_recid'])
+
+del dataset_df['c_charge_degree=M']
+
+# Change to interval threholds
+dataset_df.iloc[:, 2:5] = np.apply_along_axis(interval, 1, dataset_df.iloc[:, 2:5].values)
+dataset_df.iloc[:, 5:8] = np.apply_along_axis(interval, 1, dataset_df.iloc[:, 5:8].values)
 
 from sklearn.model_selection import KFold
 
@@ -82,7 +98,7 @@ for f_idx, (train_index, test_index) in enumerate(cv.split(dataset_df)):
 
     #%%
     # Fair Density setup
-    x_support = all_binaries(DOMAIN)
+    x_support = all_binaries(DOMAIN, threshold=[1,2])
     a_domain = [tuple(int(v) for v in t) for t in torch.eye(train_a.shape[1])]
 
     train_x_cond_a = []